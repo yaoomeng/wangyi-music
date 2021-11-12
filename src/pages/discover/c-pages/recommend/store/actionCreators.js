1440ebf48f8bf0ef159802e9fb3d388082878d4d
yurenguo <2630445979@qq.com>
2021-11-12 21:57:34 +0800

    redux hook 以及 引入immutable进行性能优化

--- src/pages/discover/c-pages/recommend/store/actionCreators.js
+++ src/pages/discover/c-pages/recommend/store/actionCreators.js
@@ -1,11 +1,16 @@
 import * as actionTypes from './constants';
+
+// getTopBanners发送网络请求，获取轮播图数据
 import { getTopBanners } from '@/services/recommend';
 
+// 返回对象
 const changeTopBannersAction = (res) => ({
     type: actionTypes.CHANGE_TOP_BANNERS,
     topBanners: res.banners
 
 })
+
+// 返回函数
 export const getTopBannerAction = () => {
     return dispatch => {
         getTopBanners().then(res => {   
